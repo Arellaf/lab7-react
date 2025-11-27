eda7c277c47964c619ed1be20783df0ecab9fb8c
Valery <valera_789@ukr.net>
2025-11-28 01:41:06 +0200

    Add login modal and registration form to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,54 +1,93 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import {
     Navbar,
     Nav,
     FormControl,
     Container,
     Form,
-    Button
+    Button,
+    Modal
 } from "react-bootstrap";
 
 import logo from "../images/logo.png";
 
-export default class Header extends Component {
-    render() {
-        return (
-            <>
-                <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
-                    <Container>
-                        <Navbar.Brand href="/">
-                            <img
-                                src={logo}
-                                height="30"
-                                width="30"
-                                className="d-inline-block align-top"
-                                alt="Logo"
-                            />{" "}
-                            React Site
-                        </Navbar.Brand>
+export default function Header() {
 
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-                        <Navbar.Collapse id="responsive-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link href="/"> Home </Nav.Link>
-                                <Nav.Link href="/about"> About us </Nav.Link>
-                                <Nav.Link href="/contacts"> Contacts </Nav.Link>
-                                <Nav.Link href="/blog"> Blog </Nav.Link>
-                            </Nav>
+    return (
+        <>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Log in</Modal.Title>
+                </Modal.Header>
 
-                            <Form className="d-flex">
-                                <FormControl
-                                    type="text"
-                                    placeholder="Search"
-                                    className="me-sm-3"
-                                />
-                                <Button variant="outline-info">Search</Button>
-                            </Form>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </>
-        );
-    }
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" className="mt-3"/>
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword" className="mt-3">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Enter password" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicCheckbox" className="mt-3">
+                            <Form.Check type="checkbox" label="Remember me" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit" className="mt-3">
+                            Submit
+                        </Button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+
+            <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img
+                            src={logo}
+                            height="30"
+                            width="30"
+                            className="d-inline-block align-top"
+                            alt="Logo"
+                        />{" "}
+                        React Site
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/"> Home </Nav.Link>
+                            <Nav.Link href="/about"> About us </Nav.Link>
+                            <Nav.Link href="/contacts"> Contacts </Nav.Link>
+                            <Nav.Link href="/blog"> Blog </Nav.Link>
+                        </Nav>
+
+                        <Form className="d-flex me-3">
+                            <FormControl
+                                type="text"
+                                placeholder="Search"
+                                className="me-sm-3"
+                            />
+                            <Button variant="outline-info">Search</Button>
+                        </Form>
+
+                        <Button className="ms-2" onClick={handleShow}>
+                            Login
+                        </Button>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
